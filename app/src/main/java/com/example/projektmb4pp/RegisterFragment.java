11eaffb39ef225e511440c85fc9c47fbd46c7005
edunni <94446799+edunni@users.noreply.github.com>
2022-11-21 17:57:12 +0100

    Added 'back' button in RegisterFragment.xml

--- app/src/main/java/com/example/projektmb4pp/RegisterFragment.java
+++ app/src/main/java/com/example/projektmb4pp/RegisterFragment.java
@@ -1,12 +1,15 @@
 package com.example.projektmb4pp;
 
+import android.media.Image;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -55,11 +58,20 @@ public class RegisterFragment extends Fragment {
         }
     }
 
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_register, container, false);
+        final ImageButton backButton = view.findViewById(R.id.backButton);
 
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_registerFragment_to_loginFragment);
+            }
+        });
 
         return view;
     }
