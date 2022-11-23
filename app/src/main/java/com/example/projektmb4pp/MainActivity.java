5e0cdcaa286ea53b604044e44955cf2414add534
edunni <94446799+edunni@users.noreply.github.com>
2022-11-23 23:12:16 +0100

    made the drawernavbar in activity main

--- app/src/main/java/com/example/projektmb4pp/MainActivity.java
+++ app/src/main/java/com/example/projektmb4pp/MainActivity.java
@@ -1,13 +1,20 @@
 package com.example.projektmb4pp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,34 @@ public class MainActivity extends AppCompatActivity {
             getSupportActionBar().hide();
         }
 
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+//        bottomNavigationView.setSelectedItemId(R.id.optionHome);
+    }
+
+    RegisterFragment registerFragment = new RegisterFragment();
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.optionHome:
+                getSupportFragmentManager().beginTransaction().replace(R.id.lol, registerFragment).commit();
+                return true;
+
+//            case R.id.optionAccount:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.navigationView, secondFragment).commit();
+//                return true;
+//
+//            case R.id.optionCart:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.navigationView, thirdFragment).commit();
+//                return true;
+//
+//            case R.id.optionSettings:
+//                getSupportFragmentManager().beginTransaction().replace(R.id.navigationView, thirdFragment).commit();
+//                return true;
+        }
+        return false;
     }
 }
