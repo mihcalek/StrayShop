a9605fd8ab5863972dafe7fb43d5c34e847a0699
edunni <94446799+edunni@users.noreply.github.com>
2022-11-23 15:29:58 +0100

    Updated template.xml and updated toolbar

---
+++ app/src/main/java/com/example/projektmb4pp/TemplateFragment.java
@@ -0,0 +1,30 @@
+package com.example.projektmb4pp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TemplateFragment extends Fragment {
+
+
+    public TemplateFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_template, container, false);
+        return view;
+    }
+}
